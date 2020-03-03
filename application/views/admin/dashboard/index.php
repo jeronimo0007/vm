0742bbff6cbd5bea0cf713347b2120d5610a6f9b
myusername <myemail>
2020-03-03 12:13:30 -0300

    Tradução variavel get usuários  para clientes cadastrados usuarios ativos para clientes ativos

--- application/views/admin/dashboard/index.php
+++ application/views/admin/dashboard/index.php
@@ -17,7 +17,7 @@
       </div><!-- /.container-fluid -->
     </div>
     <!-- /.content-header -->
-    
+
     <!-- Main content -->
     <section class="content">
       <div class="container-fluid">
